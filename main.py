f704da11966fa180a41283284a75dfff2c62f2ec
= <=>
2022-08-25 11:45:04 -0300

    add main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas            as pd
+import seaborn           as sns
+import matplotlib.pyplot as plt
+
+import matplotlib.pylab  as pylab
+
+
+from sklearn.metrics.pairwise          import cosine_similarity
+from sklearn.feature_extraction.text   import TfidfVectorizer
+
+
+df = pd.read_csv('./netflix_titles.csv')
+
+def recommendation(title):
+
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(df['description'])
+    vectorizer.get_feature_names_out()  
+
+    cos_sim = cosine_similarity(X, X)
+    indices = pd.Series(df.index, df['title'])
+
+    i = indices[title]
+    list_similarity = list(enumerate(cos_sim[i]))
+    ordenado = sorted(list_similarity, key=lambda x: x[1], reverse=True)
+    top_10 = ordenado[0:21]
+    movie_index = [movie[0] for movie in top_10]
+    return [movie for movie in df['title'].iloc[movie_index].values]
+
+def catalog():
+    return df['title']
+
+if __name__ == '__main__':
+
+    print(recommendation('Stranger Things'))
